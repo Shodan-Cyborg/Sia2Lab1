3b0ad87abcd047727c02d42aca71a25107d47f3e
Shodan-Cyborg <levantagubar3195@gmail.com>
2025-02-18 17:55:01 +0800

    second activity migration

---
+++ database/migrations/2025_02_11_074139_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('product_name', 50);
+            $table->decimal('product_price', 5,2);
+            $table->integer('product_quantity');
+            $table->integer('sales_volume');
+            $table->string('product_image', 50);
+            $table->tinyInteger('archived')->default(1);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
